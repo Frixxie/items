3b538b991dbaa3b7c8cda6246566b748a99ea068
Fredrik Hagen Fasteraune <frha@dips.no>
2024-09-12 07:50:26 +0200

    feat(backend): Add create, delete, and select by id operations for item and add profiling to endpoints

--- backend/src/item.rs
+++ backend/src/item.rs
@@ -12,28 +12,28 @@ pub struct Item {
     date_recieved: DateTime<Utc>,
 }
 
-impl Item {
-    pub fn new(
-        id: i32,
-        name: String,
-        description: String,
-        date_origin: DateTime<Utc>,
-        date_recieved: DateTime<Utc>,
-    ) -> Self {
-        Self {
-            id,
-            name,
-            description,
-            date_origin,
-            date_recieved,
-        }
-    }
+#[derive(Deserialize, Clone, Debug)]
+pub struct NewItem {
+    pub name: String,
+    pub description: String,
+    pub date_origin: DateTime<Utc>,
+    pub date_recieved: DateTime<Utc>,
+}
 
+impl Item {
     pub async fn read_from_db(pool: &PgPool) -> Result<Vec<Item>> {
         let items = sqlx::query_as::<_, Item>("SELECT * FROM items")
             .fetch_all(pool)
             .await?;
         Ok(items)
+    }
+
+    pub async fn read_from_db_by_id(pool: &PgPool, id: i32) -> Result<Item> {
+        let item = sqlx::query_as::<_, Item>("SELECT * FROM items i WHERE i.id = $1")
+            .bind(id)
+            .fetch_one(pool)
+            .await?;
+        Ok(item)
     }
 
     pub async fn insert_into_db(
@@ -46,11 +46,18 @@ impl Item {
         sqlx::query("INSERT INTO items (name, description, date_origin, date_recieved) VALUES ($1, $2, $3, $4)").bind(name).bind(description).bind(date_origin).bind(date_recieved).execute(pool).await?;
         Ok(())
     }
+
+    pub async fn delete_from_db(pool: &PgPool, id: i32) -> Result<()> {
+        sqlx::query("DELETE FROM items i WHERE i.id = $1")
+            .bind(id)
+            .execute(pool)
+            .await?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    
 
     use super::*;
     use sqlx::PgPool;
@@ -72,5 +79,55 @@ mod tests {
         assert_eq!(item.description, "Test".to_string());
         assert!((item.date_origin - now).num_seconds() < 1);
         assert!((item.date_recieved - now).num_seconds() < 1);
+    }
+
+    #[sqlx::test]
+    pub async fn select_by_id(pool: PgPool) {
+        let now = Utc::now();
+        Item::insert_into_db(&pool, "Hei", "Test", now, now)
+            .await
+            .unwrap();
+
+        let item = Item::read_from_db_by_id(&pool, 1).await;
+
+        assert!(item.is_ok());
+
+        let item = item.unwrap();
+
+        assert_eq!(item.id, 1);
+        assert_eq!(item.name, "Hei".to_string());
+        assert_eq!(item.description, "Test".to_string());
+        assert!((item.date_origin - now).num_seconds() < 1);
+        assert!((item.date_recieved - now).num_seconds() < 1);
+    }
+
+    #[sqlx::test]
+    pub async fn delete(pool: PgPool) {
+        let now = Utc::now();
+        Item::insert_into_db(&pool, "Hei", "Test", now, now)
+            .await
+            .unwrap();
+
+        let item = Item::read_from_db_by_id(&pool, 1).await;
+
+        assert!(item.is_ok());
+
+        let item = item.unwrap();
+
+        assert_eq!(item.id, 1);
+        assert_eq!(item.name, "Hei".to_string());
+        assert_eq!(item.description, "Test".to_string());
+        assert!((item.date_origin - now).num_seconds() < 1);
+        assert!((item.date_recieved - now).num_seconds() < 1);
+
+        let res = Item::delete_from_db(&pool, item.id).await;
+
+        assert!(res.is_ok());
+
+        let item = Item::read_from_db_by_id(&pool, 1).await;
+
+        dbg!(&item);
+
+        assert!(item.is_err());
     }
 }
--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,15 +1,59 @@
-use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
+use axum::{
+    extract::{Path, Request, State},
+    http::StatusCode,
+    middleware::{self, Next},
+    response::Response,
+    routing::{delete, get, post},
+    Json, Router,
+};
+use log::info;
 use sqlx::PgPool;
+use tokio::time::Instant;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
-use crate::{error::HandlerError, gifter::Gifter, item::Item, picture::Picture};
+use crate::{
+    error::HandlerError,
+    gifter::Gifter,
+    item::{Item, NewItem},
+    picture::Picture,
+};
+
+pub async fn profile_endpoint(request: Request, next: Next) -> Response {
+    let method = request.method().clone().to_string();
+    let uri = request.uri().clone();
+    info!("Handling {} at {}", method, uri);
+
+    let now = Instant::now();
+
+    let response = next.run(request).await;
+
+    let elapsed = now.elapsed();
+
+    info!(
+        "Finished handling {} at {}, used {} ms",
+        method,
+        uri,
+        elapsed.as_millis()
+    );
+    response
+}
 
 pub fn create_router(connection: PgPool) -> Router {
     Router::new()
         .route("/status/health", get(status))
         .route("/api/items", get(get_all_items))
+        .route("/api/items/:user_id", get(get_item_by_id))
+        .route("/api/item", post(add_item))
+        .route("/api/items/:user_id", delete(delete_item_by_id))
         .route("/api/pictures", get(get_all_pictures))
         .route("/api/gifters", get(get_all_gifters))
         .with_state(connection)
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(middleware::from_fn(profile_endpoint)),
+        )
 }
 
 async fn status() -> (StatusCode, String) {
@@ -21,6 +65,42 @@ async fn get_all_items(State(connection): State<PgPool>) -> Result<Json<Vec<Item
         .await
         .map_err(|e| HandlerError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
     Ok(Json(items))
+}
+
+async fn get_item_by_id(
+    State(connection): State<PgPool>,
+    Path(item_id): Path<i32>,
+) -> Result<Json<Item>, HandlerError> {
+    let item = Item::read_from_db_by_id(&connection, item_id)
+        .await
+        .map_err(|e| HandlerError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    Ok(Json(item))
+}
+
+async fn add_item(
+    State(connection): State<PgPool>,
+    Json(payload): Json<NewItem>,
+) -> Result<(), HandlerError> {
+    Item::insert_into_db(
+        &connection,
+        &payload.name,
+        &payload.description,
+        payload.date_origin,
+        payload.date_recieved,
+    )
+    .await
+    .map_err(|e| HandlerError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    Ok(())
+}
+
+async fn delete_item_by_id(
+    State(connection): State<PgPool>,
+    Path(item_id): Path<i32>,
+) -> Result<(), HandlerError> {
+    Item::delete_from_db(&connection, item_id)
+        .await
+        .map_err(|e| HandlerError::new(StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    Ok(())
 }
 
 async fn get_all_pictures(
